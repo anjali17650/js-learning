9dbdd2e8f2781487fdf6d2ec5be34bd7f386404f
anjali17650 <142310329+anjali17650@users.noreply.github.com>
2026-02-10 10:09:46 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -25,3 +25,43 @@ let someNumber = 33;
 let stringNumber =String(someNumber);
 console.log(stringNumber); 
 console.log( typeof stringNumber); 
+
+// ***********************Operations*************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+console.log(4+2);
+console.log(4-2);
+console.log(4*2);
+console.log(4/2);
+console.log(4**2);
+console.log(4%2);
+
+let str1 = "Hello";
+let str2 = "Anjali";
+let str3 = str1+ str2;
+console.log(str3);
+
+console.log("1"+2);//doesn't add =>12
+console.log(1+"2");//doesn't add =>12
+console.log("1"+2+2);//it gives 122
+console.log(1+2+"2");//it gives 32
+//instead use parenthesis
+console.log((1+2)*4);
+
+console.log(+true);//returns 1
+//console.log(true+);//gives error
+console.log(+"");//returns 0
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
