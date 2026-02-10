a4c026b25ca998bc56c29a001ad3be17dd062188
anjali17650 <142310329+anjali17650@users.noreply.github.com>
2026-02-10 07:54:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+//alert(4 +47); // we are using node.js ,not browser
+
+let name = "Anjali";
+let age = 22;
+let isLoggedIn = false;
+
+/*datatypes:
+    number => 2 to power 53
+    bigint
+    string
+    boolean => true/false
+    null => standalone value
+    undefined =>value is not been assigned
+    symbol => unique
+    object
+*/
+console.log(typeof undefined);
