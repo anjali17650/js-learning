df86bd01f08e28f53c6487e32aa0a028ec11c0ef
anjali17650 <142310329+anjali17650@users.noreply.github.com>
2026-02-10 09:42:56 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score = "33abs";
+score = null;
+score = undefined;
+score =true;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number (score);
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+//"33" => 33
+//"33abs" => NaN
+//true =>1; false =>0
+
+let isLoggedIn = 0;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" =>false
+//"Anjali" =>true
+
+let someNumber = 33;
+let stringNumber =String(someNumber);
+console.log(stringNumber); 
+console.log( typeof stringNumber); 
